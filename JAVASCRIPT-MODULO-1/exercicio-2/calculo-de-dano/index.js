f946ca88d99242eeca393c9a0a5d59250c3fc7ec
auyber <160794306+auyber@users.noreply.github.com>
2024-11-12 12:48:35 -0300

    feat: update ex Calculo-de-dano

--- JAVASCRIPT-MODULO-1/exercicio-2/calculo-de-dano/index.js
+++ JAVASCRIPT-MODULO-1/exercicio-2/calculo-de-dano/index.js
@@ -1,4 +1,4 @@
-const nome = prompt("Qual seu nome?")
+/*const nome = prompt("Qual seu nome?")
 const ataque1 = prompt("Qual seu poder de ataque?")
 
 const nome2 = prompt("Qual nome do seu inimigo?")
@@ -14,4 +14,31 @@ if (ataque1 > defesa && escudo == "não") {
     alert("Dano causado: 0")
 }
 
-alert()
+alert()*/
+
+//Resolução
+
+const atacante = prompt("Qual é o nome do personagem atacante?")
+const poderDeAtaque = parseFloat(prompt("Qual é o seu poder de ataque?"))
+
+const defensor = prompt("Qual é nome do personagem defensor?")
+let pontosDeVida = parseFloat(prompt("Quantos pontos de vida ele possui?"))
+const poderDeDefesa = parseFloat(prompt("Qual é o seu poder de defesa?"))
+const possuiEscudo = prompt("Ele possui um escudo? (Sim/Não)")
+
+let danoCausado = 0
+
+if (poderDeAtaque > poderDeDefesa && possuiEscudo === "Não") {
+    danoCausado = poderDeAtaque - poderDeDefesa
+  } else if (poderDeAtaque > poderDeDefesa && possuiEscudo === "Sim") {
+    danoCausado = (poderDeAtaque - poderDeDefesa) / 2
+  }
+  
+  pontosDeVida -= danoCausado
+
+  alert(atacante + " causou " + danoCausado + " pontos de dano em " + defensor)
+alert(
+  atacante + "\nPoder de ataque: " + poderDeAtaque + "\n\n" +
+  defensor + "\nPontos de vida: " + pontosDeVida +
+  "\nPoder de defesa: " + poderDeDefesa + "\nPossui escudo: " + possuiEscudo
+)
