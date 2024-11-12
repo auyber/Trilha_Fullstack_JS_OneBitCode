c57ad8b306604a9d6e3cd8e3b305bd59b9864eae
auyber <160794306+auyber@users.noreply.github.com>
2024-11-12 12:21:42 -0300

    feat: create ex teste de velocidade

---
+++ JAVASCRIPT-MODULO-1/exercicio-2/teste-de-velocidade/index.js
@@ -0,0 +1,8 @@
+const veiculo1 = prompt("Qual a velocidade do veículo 1: ")
+const veiculo2 = prompt("Qual a velocidade do veículo 2: ")
+
+if (veiculo1 > veiculo2) {
+    alert("Veículo 1 é mais rápido")
+} else if (veiculo1 < veiculo2) {
+    alert("Veículo 2 é mais rápido")
+} 
