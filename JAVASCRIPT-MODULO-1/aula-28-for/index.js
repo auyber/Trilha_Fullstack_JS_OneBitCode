4dc1ba7ede2d60c0f3a87f5b0d504caf5ad9fc2b
auyber <160794306+auyber@users.noreply.github.com>
2024-11-18 11:33:43 -0300

    feat: create for

---
+++ JAVASCRIPT-MODULO-1/aula-28-for/index.js
@@ -0,0 +1,14 @@
+//inicialização; condição; finalização
+/*for (let indice = 0; indice <= 10; indice++) {
+    alert("Indice = " + indice )
+}
+
+podemos fazer o for sem dizer nada, para isso tem que deixar o ;
+for (; indice <= 10;)
+*/
+
+let nome = "Auyber Genesini"
+
+for (let indice = 0; indice < nome.length; indice++) {
+    console.log(nome[indice])
+}
