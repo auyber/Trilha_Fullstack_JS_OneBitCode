57048b18a97e703502fedb99d06c1b387fc8deb5
auyber <160794306+auyber@users.noreply.github.com>
2024-11-19 11:18:32 -0300

    feat: create aula-32-arrays

---
+++ JAVASCRIPT-MODULO-1/aula-32-array/index.js
@@ -0,0 +1,10 @@
+const listaDeCompras = ["Pimenta", false]
+console.log(listaDeCompras)
+
+listaDeCompras[0] = "Picanha"
+listaDeCompras[1] = "Arroz"
+listaDeCompras[5] = "Feijão"
+
+console.log(listaDeCompras)
+
+console.log(listaDeCompras[3])
