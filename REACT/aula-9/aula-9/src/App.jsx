e161adb8300ef57cd7d4c8775bf1a5cb4da6dd66
auyber <160794306+auyber@users.noreply.github.com>
2025-03-13 08:17:26 -0300

    feat: create aula-9

--- REACT/aula-9/aula-9/src/App.jsx
+++ REACT/aula-9/aula-9/src/App.jsx
@@ -1,10 +1,18 @@
+import StatusText from "./assets/components/StatusText"
+import { Subtitle } from "./assets/components/Subtitle"
+import Title from "./assets/components/Title"
+
+function sun(a, b) {
+  return a + b
+}
+
 export default function App() {
-  const tech = "JS"
   
   return (
     <div>
-      <h1>{tech} is Awesome!!!</h1>
-      <h2>It's easy, Like 1 + 1 is {1 + 1}</h2>
+      <Title />
+      <Subtitle />
+      <StatusText />
     </div>
   )
 }
---
+++ REACT/aula-9/aula-9/src/assets/components/Title/index.jsx
@@ -0,0 +1,5 @@
+//PascalCase
+export default function Title() {
+    const tech = "JavaScript"
+    return <h1>{tech} is Awesome!!!</h1>
+  }
