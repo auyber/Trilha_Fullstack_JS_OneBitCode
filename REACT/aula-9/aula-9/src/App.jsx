687b258610963d288e1586c12f435eca150a1d08
auyber <160794306+auyber@users.noreply.github.com>
2025-03-13 07:39:57 -0300

    feat: create aula-9

---
+++ REACT/aula-9/aula-9/src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  const tech = "JS"
+  
+  return (
+    <div>
+      <h1>{tech} is Awesome!!!</h1>
+      <h2>It's easy, Like 1 + 1 is {1 + 1}</h2>
+    </div>
+  )
+}
