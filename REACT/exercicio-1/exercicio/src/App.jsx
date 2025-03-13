a544a2d8d07fcae4e4800e9483687454defb38e1
auyber <160794306+auyber@users.noreply.github.com>
2025-03-13 07:24:40 -0300

    feat: create exercicio-1

---
+++ REACT/exercicio-1/exercicio/src/App.jsx
@@ -0,0 +1,33 @@
+export default function App () {
+  return (
+    <div className="app">
+      <div>
+      <img src="/react.png" alt="logo do react" />
+        <h1>React</h1>
+        <p>A biblioteca para interface de usuário web e nativas</p>
+        <button>Aprenda React</button><button>Referência da API</button>
+      </div>
+      <hr />
+      <div>
+        <h2>Crie interfaces de usuário de componentes</h2>
+        <p>React permite que voçê construa interfaces de usuário a partir de pedaços indiviaduais chamados componentes</p>
+      </div>
+      <hr />
+      <div>
+        <h2>Escreva componentes com código e marcação</h2>
+        <p>Componentes React são funções JavaScript. A sintaxe de marcação é chamada de JSX. É uma extensão da sintaxe JavaScript popularizada pelo React</p>
+      </div>
+      <div>
+        <h2>Próximos passos</h2>
+        <ul>
+          <li>Usos de dados dinâmicos no JSX</li>
+          <li>Criação de novos componentes</li>
+          <li>Estilização de componentes</li>
+          <li>Reutilização de componentes</li>
+          <li>Uso de props e children</li>
+          <li>Uso de eventos do JavaScript</li>
+        </ul>
+        </div>
+    </div>
+  )
+}
