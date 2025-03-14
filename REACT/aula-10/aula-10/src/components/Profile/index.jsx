975daeec9182a06d352bf654540f912a604ff389
auyber <160794306+auyber@users.noreply.github.com>
2025-03-14 07:36:12 -0300

    feat: create aula-10

---
+++ REACT/aula-10/aula-10/src/components/Profile/index.jsx
@@ -0,0 +1,26 @@
+import LinkButton from "../LinkButton"
+import ProfileSection from "../ProfileSection"
+import Title from "../Title"
+import styles from "./styles.module.css"
+
+export default function Profile(props) {
+  return (
+    <div className={styles.container}>
+      <img className={styles.avatar} src={props.avatar} alt={props.name} />
+      <Title >
+        <span>{props.name}</span>
+        <button>Follow</button>
+      </Title>
+      <ProfileSection>{props.bio}</ProfileSection> 
+      <ProfileSection>{props.phone}</ProfileSection>
+      <ProfileSection>{props.email}</ProfileSection>
+      <ProfileSection>
+        <div className={styles.links}>
+          <LinkButton href={props.githubUrl} target="_blank">GitHub</LinkButton>
+          <LinkButton href={props.linkedinUrl} target="_blank">LinkedIn</LinkButton>
+          <LinkButton href={props.twitterUrl} target="_blank">Twitter</LinkButton>
+        </div>
+      </ProfileSection>
+    </div>
+  )
+}
