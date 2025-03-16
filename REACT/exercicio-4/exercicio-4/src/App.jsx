09ab000ae89aae98d4fd2d2d421494ee4c496266
auyber <160794306+auyber@users.noreply.github.com>
2025-03-16 13:14:44 -0300

    feat: update share state

--- REACT/exercicio-4/exercicio-4/src/App.jsx
+++ REACT/exercicio-4/exercicio-4/src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react"
+import Input from "./components/input"
 
 function App() {
   const [password, setPassword] = useState("")
-  const [passwordSize, setPasswordSize] = useState(12)
   const [copyText, setCopyText] = useState("Copiar")
+  const [passwordSize, setPasswordSize] = useState(12)
 
   function generate() {
     const characters = "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?"
@@ -26,13 +27,7 @@ function App() {
       <h1>Gerador de senhas</h1>
       <div>
         <label htmlFor="passwordSize">Tamanho: </label>
-        <input
-          type="number"
-          id="passwordSize"
-          min={1}
-          value={passwordSize}
-          onChange={(ev) => setPasswordSize(ev.target.value)}
-        />
+        <Input passwordSize={passwordSize} setPasswordSize={setPasswordSize}/>
       </div>
       <button onClick={generate}>Gerar senha de {passwordSize} caracteres</button>
       <button onClick={copyToClipboard}>{copyText}</button>
